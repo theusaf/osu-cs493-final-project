9110221138e4c73636fc493a8e971e26c25f2b71
berginp <berginp@oregonstate.edu>
2024-06-05 13:36:08 -0700

    commiting

--- src/api/assignments.ts
+++ src/api/assignments.ts
@@ -1,5 +1,13 @@
-import { Router } from "express";
+import { Router, Request, Response, NextFunction} from "express";
+import {collection} from "firebase/firestore";
+import {db} from "./firebaseConfig";
+import { requiredInBody } from "./middleware.js";
 
 const router = Router();
+
+router.post('/assignments', requiredInBody(["courseID", "title", "points", "due"]), (req: Request, res: Response) => {
+    const assignmentsCollection = collection(db, 'Assignments');
+
+})
 
 export default router;
