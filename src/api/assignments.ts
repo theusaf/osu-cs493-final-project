51fdd9f094492f2b7349e17bbb2a48c168918034
Daniel Lau <32113157+theusaf@users.noreply.github.com>
2024-06-07 10:41:39 -0700

    feat: stub api routes (#27)
    * feat: stub user routes
    
    * feat: stub assignment routes
    
    * feat: stub course routes

--- src/api/assignments.ts
+++ src/api/assignments.ts
@@ -2,4 +2,61 @@ import { Router } from "express";
 
 const router = Router();
 
+router.post("/:id/submissions", (req, res) => {
+  const assignmentId = req.params.id;
+  const submissionData = req.body;
+  // TODO: Implement
+  res.status(201).json({
+    id: 123,
+  });
+});
+
+router.get("/:id/submissions", (req, res) => {
+  const assignmentId = req.params.id;
+  // TODO: Implement
+  res.json({
+    submissions: [
+      {
+        assignmentId: 123,
+        studentId: 123,
+        timestamp: "2022-06-14T17:00:00-07:00",
+        grade: 94.5,
+        file: "string",
+      },
+    ],
+  });
+});
+
+router.delete("/:id", (req, res) => {
+  const assignmentId = req.params.id;
+  // TODO: Implement
+  res.status(204).send();
+});
+
+router.patch("/:id", (req, res) => {
+  const assignmentId = req.params.id;
+  const assignmentData = req.body;
+  // TODO: Implement
+  res.status(200).send();
+});
+
+router.get("/:id", (req, res) => {
+  const assignmentId = req.params.id;
+  // TODO: Implement
+  res.json({
+    courseId: 123,
+    title: "Assignment 3",
+    points: 100,
+    due: "2022-06-14T17:00:00-07:00",
+  });
+});
+
+router.post("/", (req, res) => {
+  const assignmentData = req.body;
+  // TODO: Implement
+  res.status(201).json({
+    id: 123,
+  });
+});
+
 export default router;
--- src/api/users.ts
+++ src/api/users.ts
@@ -2,4 +2,31 @@ import { Router } from "express";
 
 const router = Router();
 
+router.get("/:id", (req, res) => {
+  const userId = req.params.id;
+  // TODO: Implement
+  res.json({
+    name: "Jane Doe",
+    email: "doej@oregonstate.edu",
+    password: "hunter2",
+    role: "student",
+  });
+});
+
+router.post("/", (req, res) => {
+  const userData = req.body;
+  // TODO: Implement
+  res.status(201).json({
+    id: 123,
+  });
+});
+
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  // TODO: Implement
+  res.json({
+    token: "aaaaaaaa.bbbbbbbb.cccccccc",
+  });
+});
+
 export default router;
