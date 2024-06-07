dbfe6afb5d6db67eff215a7691eba22527289100
theusaf <theusafyt@gmail.com>
2024-06-06 20:06:56 -0700

    feat: stub user routes

--- src/api/users.ts
+++ src/api/users.ts
@@ -2,4 +2,28 @@ import { Router } from "express";
 
 const router = Router();
 
+router.get("/:id", (req, res) => {
+  const userId = req.params.id;
+  res.json({
+    name: "Jane Doe",
+    email: "doej@oregonstate.edu",
+    password: "hunter2",
+    role: "student",
+  });
+});
+
+router.post("/", (req, res) => {
+  const userData = req.body;
+  res.status(201).json({
+    id: 123,
+  });
+});
+
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  res.json({
+    token: "aaaaaaaa.bbbbbbbb.cccccccc",
+  });
+});
+
 export default router;
