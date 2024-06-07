bc2b2b9111be6798668de2196d9cb90ca616b548
berginp <berginp@oregonstate.edu>
2024-06-07 13:39:45 -0700

    add imports

--- src/api/assignments.ts
+++ src/api/assignments.ts
@@ -1,6 +1,5 @@
 import { Router, Request, Response, NextFunction} from "express";
 import {collection} from "firebase/firestore";
-import {db} from "./firebaseConfig";
 import { requiredInBody } from "../util/middleware.js";
 import { requireAuthentication } from "../util/authentication.js";
 
@@ -8,6 +7,8 @@ const router = Router();
 
 router.post('/assignments', requireAuthentication, requiredInBody(["courseID", "title", "points", "due"]), (req: Request, res: Response) => {
     //const assignmentsCollection = collection(db, 'Assignments');
+    //select course from courseID
+    //compare req.user to course teacher ID
     return res.status(200);
 
 });
