e6660dfd67215c6ae3a09622d04db7413c8d8b45
Daniel Lau <32113157+theusaf@users.noreply.github.com>
2024-06-07 14:26:14 -0700

    feat: implement account creation and login (#30)
    * fix(model): save new id in database after creation
    
    * feat(requireAuthentication): adds user to authentication
    
    * feat: implement login route
    
    * feat: implement user creation path

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,4 +1,11 @@
 import { Router } from "express";
+import {
+  PartiallyAuthenticatedRequest,
+  createSessionToken,
+  hash,
+  verify,
+} from "../util/authentication.js";
+import { User, UserType } from "../models/users.js";
 
 const router = Router();
 
@@ -13,19 +20,61 @@ router.get("/:id", (req, res) => {
   });
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req: PartiallyAuthenticatedRequest, res) => {
+  const loggedUserId = req.userId;
   const userData = req.body;
-  // TODO: Implement
+  if (
+    typeof userData.name !== "string" ||
+    typeof userData.email !== "string" ||
+    typeof userData.password !== "string" ||
+    typeof userData.role !== "string"
+  ) {
+    return res.status(400).json({
+      error: "Invalid data",
+    });
+  }
+  if (!["admin", "instructor", "student"].includes(userData.role)) {
+    return res.status(400).json({
+      error: "Invalid data",
+    });
+  }
+  if (["admin", "instructor"].includes(userData.role)) {
+    const sendUnauthorized = () => {
+      res.status(403).json({
+        error: "Unauthorized",
+      });
+    };
+    if (!loggedUserId) return sendUnauthorized();
+    const user = await User.findById(loggedUserId);
+    if (user.role !== "admin") return sendUnauthorized();
+  }
+  const newUser = new User({
+    name: userData.name,
+    email: userData.email,
+    password: await hash(userData.password),
+    role: userData.role,
+  });
+  await newUser.save();
   res.status(201).json({
-    id: 123,
+    id: newUser.id,
   });
 });
 
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
   const { email, password } = req.body;
-  // TODO: Implement
+  const sendError = () => {
+    res.status(401).json({
+      error: "Invalid email or password",
+    });
+  };
+  if (typeof email !== "string" || typeof password !== "string") {
+    return sendError();
+  }
+  const [user] = await User.findAll({ where: { email }, limit: 1 });
+  if (!user) return sendError();
+  if (!(await verify(user.password, password))) return sendError();
   res.json({
-    token: "aaaaaaaa.bbbbbbbb.cccccccc",
+    token: createSessionToken(user.id!),
   });
 });
 
