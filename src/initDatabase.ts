ec9604510b3673863cd8f511eef3e531d82a36c1
Shane Ludwig <shane.ludwig01@gmail.com>
2024-06-08 14:38:42 -0700

    feat: add database initialization (#37)
    * API courses get course roster by course id maybe complete? Needs review
    
    * Refactor get course roster by id
    
    * Created initialization for database. Needs to be refactored

---
+++ src/initDatabase.ts
@@ -0,0 +1,34 @@
+import {connection } from "./firebase.js";
+import fs from 'fs';
+import path from 'path';
+import { FirestoreCollection  } from "./util/constants.js"
+
+
+const checkData = async (collectionName: string, jsonFilePath: string) => {
+    const collectionRef = connection.collection(collectionName);
+    const jsonData = JSON.parse(fs.readFileSync(jsonFilePath, 'utf-8'))
+
+    for (const doc of jsonData) {
+        const docRef = collectionRef.doc(doc.id);
+        const docSnapshot = await docRef.get();
+
+        if (!docSnapshot.exists) {
+            await docRef.set(doc);
+            console.log(`Document ${doc.id} added to collection`);
+        } else {
+            console.log(`Document ${doc.id} already exists in collection`);
+        }
+    }
+};
+
+
+export const initDatabase = async () => {
+    try {
+        await checkData(FirestoreCollection.USERS, path.join(__dirname, 'users.json'));
+        await checkData(FirestoreCollection.ASSIGNMENTS, path.join(__dirname, 'assignments.json'));
+        await checkData(FirestoreCollection.COURSES, path.join(__dirname, 'courses.json'));
+    } catch (error) {
+        console.error('Error initializing database:', error)
+    }
+}
+
