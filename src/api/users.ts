6ce4709c22d13d2a2a5244ad0293ec78682c91b1
theusaf <theusafyt@gmail.com>
2024-06-07 19:25:58 -0700

    perf(users): add required field middleware

--- src/api/users.ts
+++ src/api/users.ts
@@ -5,7 +5,8 @@ import {
   hash,
   verify,
 } from "../util/authentication.js";
-import { User, UserType } from "../models/users.js";
+import { User } from "../models/users.js";
+import { requiredInBody } from "../util/middleware.js";
 
 const router = Router();
 
@@ -20,62 +21,70 @@ router.get("/:id", (req, res) => {
   });
 });
 
-router.post("/", async (req: PartiallyAuthenticatedRequest, res) => {
-  const loggedUserId = req.userId;
-  const userData = req.body;
-  if (
-    typeof userData.name !== "string" ||
-    typeof userData.email !== "string" ||
-    typeof userData.password !== "string" ||
-    typeof userData.role !== "string"
-  ) {
-    return res.status(400).json({
-      error: "Invalid data",
+router.post(
+  "/",
+  requiredInBody(["name", "email", "password", "role"]),
+  async (req: PartiallyAuthenticatedRequest, res) => {
+    const loggedUserId = req.userId;
+    const userData = req.body;
+    if (
+      typeof userData.name !== "string" ||
+      typeof userData.email !== "string" ||
+      typeof userData.password !== "string" ||
+      typeof userData.role !== "string"
+    ) {
+      return res.status(400).json({
+        error: "Invalid data",
+      });
+    }
+    if (!["admin", "instructor", "student"].includes(userData.role)) {
+      return res.status(400).json({
+        error: "Invalid data",
+      });
+    }
+    if (["admin", "instructor"].includes(userData.role)) {
+      const sendUnauthorized = () => {
+        res.status(403).json({
+          error: "Unauthorized",
+        });
+      };
+      if (!loggedUserId) return sendUnauthorized();
+      const user = await User.findById(loggedUserId);
+      if (user.role !== "admin") return sendUnauthorized();
+    }
+    const newUser = new User({
+      name: userData.name,
+      email: userData.email,
+      password: await hash(userData.password),
+      role: userData.role,
     });
-  }
-  if (!["admin", "instructor", "student"].includes(userData.role)) {
-    return res.status(400).json({
-      error: "Invalid data",
+    await newUser.save();
+    res.status(201).json({
+      id: newUser.id,
     });
-  }
-  if (["admin", "instructor"].includes(userData.role)) {
-    const sendUnauthorized = () => {
-      res.status(403).json({
-        error: "Unauthorized",
+  },
+);
+
+router.post(
+  "/login",
+  requiredInBody(["email", "password"]),
+  async (req, res) => {
+    const { email, password } = req.body;
+    const sendError = () => {
+      res.status(401).json({
+        error: "Invalid email or password",
       });
     };
-    if (!loggedUserId) return sendUnauthorized();
-    const user = await User.findById(loggedUserId);
-    if (user.role !== "admin") return sendUnauthorized();
-  }
-  const newUser = new User({
-    name: userData.name,
-    email: userData.email,
-    password: await hash(userData.password),
-    role: userData.role,
-  });
-  await newUser.save();
-  res.status(201).json({
-    id: newUser.id,
-  });
-});
-
-router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  const sendError = () => {
-    res.status(401).json({
-      error: "Invalid email or password",
+    if (typeof email !== "string" || typeof password !== "string") {
+      return sendError();
+    }
+    const [user] = await User.findAll({ where: { email }, limit: 1 });
+    if (!user) return sendError();
+    if (!(await verify(user.password, password))) return sendError();
+    res.json({
+      token: createSessionToken(user.id!),
     });
-  };
-  if (typeof email !== "string" || typeof password !== "string") {
-    return sendError();
-  }
-  const [user] = await User.findAll({ where: { email }, limit: 1 });
-  if (!user) return sendError();
-  if (!(await verify(user.password, password))) return sendError();
-  res.json({
-    token: createSessionToken(user.id!),
-  });
-});
+  },
+);
 
 export default router;
