791c003574971fd08f81d931c588dd4a7540b6f7
theusaf <theusafyt@gmail.com>
2024-06-10 15:50:26 -0700

    style: format code

--- test/student/main.test.js
+++ test/student/main.test.js
@@ -35,24 +35,26 @@ describe("Student", async () => {
     assert.strictEqual(data.name, "Shane Ludwig");
     assert.strictEqual(data.email, "shane@oregonstate.edu");
     assert.strictEqual(data.role, "student");
-    assert.deepEqual(data.courses, [])
+    assert.deepEqual(data.courses, []);
     assert.ok(Array.isArray(data.courses));
   });
 
   await test("Create a new submission for an assignment", async () => {
     const assignmentId = "7";
-    const response = await displayFetch(`${API_BASE}/assignments/${assignmentId}/submissions`, {
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${studentToken}`
+    const response = await displayFetch(
+      `${API_BASE}/assignments/${assignmentId}/submissions`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${studentToken}`,
+        },
+        method: "POST",
+        body: JSON.stringify({
+          timestamp: "2024-06-09T12:00:00Z",
+        }),
       },
-      method: "POST",
-      body: JSON.stringify({
-        timestamp: "2024-06-09T12:00:00Z"
-      })
-    });
+    );
     const data = await response.json();
     assert.strictEqual(response.status, 201); // assuming successful creation returns 201 Created
   });
 });
-
