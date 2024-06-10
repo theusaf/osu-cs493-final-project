628d17cb1fc29302d1602dea2d198d6cd61033de
Shane Ludwig <shane.ludwig01@gmail.com>
2024-06-10 15:49:47 -0700

    test: add some student-specific tests (#54)

--- test/student/main.test.js
+++ test/student/main.test.js
@@ -20,4 +20,39 @@ describe("Student", async () => {
     assert.strictEqual(typeof data.token, "string");
     studentToken = data.token;
   });
+
+  await test("Fetch specific user data", async () => {
+    const userId = "202"; // Can be replaced with any user id
+    const response = await displayFetch(`${API_BASE}/users/${userId}`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${studentToken}`,
+      },
+      method: "GET",
+    });
+    const data = await response.json();
+    assert.strictEqual(response.status, 200);
+    assert.strictEqual(data.name, "Shane Ludwig");
+    assert.strictEqual(data.email, "shane@oregonstate.edu");
+    assert.strictEqual(data.role, "student");
+    assert.deepEqual(data.courses, [])
+    assert.ok(Array.isArray(data.courses));
+  });
+
+  await test("Create a new submission for an assignment", async () => {
+    const assignmentId = "7";
+    const response = await displayFetch(`${API_BASE}/assignments/${assignmentId}/submissions`, {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${studentToken}`
+      },
+      method: "POST",
+      body: JSON.stringify({
+        timestamp: "2024-06-09T12:00:00Z"
+      })
+    });
+    const data = await response.json();
+    assert.strictEqual(response.status, 201); // assuming successful creation returns 201 Created
+  });
 });
+
